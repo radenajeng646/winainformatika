2c5f6399034873b19271dcab2277eb65739b821a
radenajeng646 <anurwinasih@gmail.com>
2025-05-20 08:22:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("🩷 I love lana")
 st.write(
-    "Lana is tthe best guyss, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Lana is tthe best guyss"
 )
 st.image("#Pak_Solomon.jpeg")
