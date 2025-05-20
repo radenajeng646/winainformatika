e5179c54f03e4acc276ea3b8c813b074185a59dd
radenajeng646 <anurwinasih@gmail.com>
2025-05-20 07:49:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 I love lana")
+st.title("🩷 I love lana")
 st.write(
     "Lana is tthe best guyss, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
